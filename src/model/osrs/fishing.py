cdef57f60005cdef7ce50e8ac05df03057b95b21
theloniusfunk <118133495+theloniusfunk@users.noreply.github.com>
2023-01-20 21:47:06 -0500

    update

---
+++ src/model/osrs/fishing.py
@@ -0,0 +1,110 @@
+import time
+
+import pyautogui as pag
+import utilities.random_util as rd
+
+import utilities.color as clr
+from model.bot import BotStatus
+from model.osrs.osrs_bot import OSRSBot
+from utilities.api.status_socket import StatusSocket
+from utilities.api.morg_http_client import MorgHTTPSocket
+from utilities.geometry import Point, RuneLiteObject
+
+
+
+class OSRSFishing(OSRSBot):
+    def __init__(self):
+        title = "Fishing"
+        description = "This bot fishes... fish. Take out a rod and bait, position your character " + "near a tagged fishing spot, and press play."
+        super().__init__(bot_title=title, description=description)
+        self.running_time = 50
+        self.protect_slots = 1
+
+    def create_options(self):
+        self.options_builder.add_slider_option("running_time", "How long to run (minutes)?", 1, 500)
+        self.options_builder.add_slider_option("protect_slots", "When dropping, protect first x slots:", 0, 4)
+
+    def save_options(self, options: dict):
+        for option in options:
+            if option == "running_time":
+                self.running_time = options[option]
+            elif option == "protect_slots":
+                self.protect_slots = options[option]
+            else:
+                self.log_msg(f"Unknown option: {option}")
+                print("Developer: ensure that the option keys are correct, and that options are being unpacked correctly.")
+                self.options_set = False
+                return
+        self.log_msg(f"Bot will run for {self.running_time} minutes.")
+        self.log_msg(f"Protecting first {self.protect_slots} slots when dropping inventory.")
+        self.log_msg("Options set successfully.")
+        self.options_set = True
+
+    def main_loop(self):  # sourcery skip: low-code-quality, use-named-expression
+        # API setup
+        api_s = StatusSocket()
+        api_m = MorgHTTPSocket()
+
+        # Client setup
+        #self.set_camera_zoom(50)
+
+        #self.log_msg("Selecting inventory...")
+        #self.mouse.move_to(self.win.cp_tabs[3].random_point())
+        #self.mouse.click()
+
+        #time.sleep(0.5)
+        #self.disable_private_chat()
+        #time.sleep(0.5)
+
+        # Set compass
+        #self.set_compass_north()
+        #self.move_camera_up()
+
+        fished = 0
+        failed_searches = 0
+
+        # Main loop
+        start_time = time.time()
+        end_time = self.running_time * 60
+        while time.time() - start_time < end_time:
+            # Check to drop inventory
+            if api_s.get_is_inv_full():
+                self.take_break(max_seconds=3)
+                #rd.sleep_random(1.2,2)
+                self.drop_all(skip_slots=list(range(self.protect_slots)))
+                fished += 28 - self.protect_slots
+                self.log_msg(f"Fishes fished: ~{fished}")
+                #time.sleep(2)
+
+            # If not fishing, click fishing spot
+           
+            while not self.is_player_doing_action("Fishing"):
+                self.take_break(max_seconds=1)
+                spot = self.get_nearest_tag(clr.CYAN)
+                if spot is None:
+                    failed_searches += 1
+                    time.sleep(2)
+                    if failed_searches > 10:
+                        self.log_msg("Failed to find fishing spot.")
+                        self.set_status(BotStatus.STOPPED)
+                        return
+                else:
+                    self.log_msg("Clicking fishing spot...")
+                    self.mouse.move_to(spot.random_point())
+                    pag.click()
+                    time.sleep(1)
+                    break
+             
+            while not api_m.get_is_player_idle():
+                time.sleep(1)
+                print("not idle")
+            print("idle")    
+            time.sleep(3)
+
+            # Update progress
+            self.update_progress((time.time() - start_time) / end_time)
+
+        self.update_progress(1)
+        self.log_msg("Finished.")
+        self.logout()
+        self.set_status(BotStatus.STOPPED)
--- src/model/osrs/woodcutter.py
+++ src/model/osrs/woodcutter.py
@@ -3,11 +3,13 @@ import time
 import utilities.api.item_ids as ids
 import utilities.color as clr
 import utilities.random_util as rd
+import pyautogui as pag
 from model.osrs.osrs_bot import OSRSBot
 from model.runelite_bot import BotStatus
 from utilities.api.morg_http_client import MorgHTTPSocket
 from utilities.api.status_socket import StatusSocket
 from utilities.geometry import RuneLiteObject
+import utilities.imagesearch as imsearch
 
 
 class OSRSWoodcutter(OSRSBot):
@@ -15,8 +17,8 @@ class OSRSWoodcutter(OSRSBot):
         bot_title = "Woodcutter"
         description = "This bot power-chops wood. Position your character near some trees, tag them, and press the play button."
         super().__init__(bot_title=bot_title, description=description)
-        self.running_time = 1
-        self.take_breaks = False
+        self.running_time = 50
+        self.take_breaks = True
 
     def create_options(self):
         self.options_builder.add_slider_option("running_time", "How long to run (minutes)?", 1, 500)
@@ -42,10 +44,10 @@ class OSRSWoodcutter(OSRSBot):
         # Setup API
         api_m = MorgHTTPSocket()
         api_s = StatusSocket()
-
-        self.log_msg("Selecting inventory...")
-        self.mouse.move_to(self.win.cp_tabs[3].random_point())
-        self.mouse.click()
+        
+        #self.log_msg("Selecting inventory...")
+        #self.mouse.move_to(self.win.cp_tabs[3].random_point())
+        #self.mouse.click()
 
         self.logs = 0
         failed_searches = 0
@@ -53,49 +55,185 @@ class OSRSWoodcutter(OSRSBot):
         # Main loop
         start_time = time.time()
         end_time = self.running_time * 60
+        
+        
         while time.time() - start_time < end_time:
             # 5% chance to take a break between clicks
             if rd.random_chance(probability=0.05) and self.take_breaks:
                 self.take_break(max_seconds=15)
 
-            # 2% chance to drop logs early
-            if rd.random_chance(probability=0.02):
+                """ # 2% chance to drop logs early
+                if rd.random_chance(probability=0.02):
                 self.__drop_logs(api_s)
 
-            # If inventory is full, drop logs
+                # If inventory is full, drop logs
+                if api_s.get_is_inv_full():
+                self.__drop_logs(api_s) """
+            
+            print("check if inv full")
+            # If inventory is full, go bank
             if api_s.get_is_inv_full():
-                self.__drop_logs(api_s)
+                print("inv full")
+                self.take_break(max_seconds=3)
+                cyan_tile = self.get_nearest_tag(clr.CYAN)
+                self.mouse.move_to(cyan_tile.random_point())
+                pag.click()
+
+                #running to bank
+                while not api_m.get_is_player_idle():
+                    time.sleep(0.5)
+                    print("not idle - running")
+                print("idle - at bank")    
+                #self.take_break(max_seconds=1)
+
+                if DepositAll:= imsearch.search_img_in_rect(imsearch.BOT_IMAGES.joinpath("bank", "DepositAll.png"), self.win.game_view, 0.05):
+                    print("deposit all")
+                    self.mouse.move_to(DepositAll.random_point())
+                    self.mouse.click()
+                    self.take_break(max_seconds=1)
+                    print("close bank")
+                    pag.press('esc')
+                    time.sleep(0.1)
+                #self.take_break(max_seconds=1)
+
+
+                #slots = api_s.get_inv_item_indices(ids.logs)
+                #self.mouse.move_to(self.win.inventory_slots[0].random_point)
+                #pag.keyDown("Shift")
+                #time.sleep(0.3)
+                #pag.click()
+                #time.sleep(0.3)
+                #pag.keyUp("Shift")
+                #pag.press('esc')
+                #time.sleep(2) 
+                #while api_s.get_is_inv_full():
+                 #   print("75")
+                  #  cyan_tile = self.get_nearest_tag(clr.CYAN)
+                   # print(cyan_tile)
+                    # While we cannot see the blue tile, move camera around
+                 #   ...
+                   #failed_bank_searches += 1
+                   #if failed_bank_searches >= 10:
+                       # Can't find bank, abort
+                    #  self.stop()
+                 # If the code makes it here, that means we've found the blue tile
+                 # Probably don't need to check the mouseover as your char will be stationary,
+                 # can if u want tho.                
+                 # # Click if the mouseover text assures us we're clicking the bank
+                 #if not self.mouseover_text(contains="Bank"):
+                 #   continue
+                 #failed_bank_searches = 0
+                
+                   # self.mouse.move_to(cyan_tile.random_point())
+                    #pag.click()
+                    #time.sleep(2) 
+
+                    #while not api_m.get_is_player_idle():
+                    # While player is running to the bank, wait
+                     #   time.sleep(1)
+                      #  print("not idle")
+                    # If the code makes it here, assume player has opened the bank.
+                    #slots = api_s.get_inv_item_indices(ids.logs)
+                    #print(slots)
+                    #STOPPED HERE
+                    #self.mouse.move_to(self.win.inventory_slots[0].random_point)
+                    #pag.keyDown("Shift")
+                    #pag.click()
+                    #pag.keyUp("Shift")
+                    #rd.sleep_random(1.5,2)
+                    
+                    #pag.press('esc')
+                    #time.sleep(2) 
+                    #rd.sleep_random(1.5,2) 
+                
+                #slots = api_s.get_inv_item_indices(ids.logs)
+                #if not slots:
+                 #   # There are no logs in your inventory, wtf?
+                #    self.stop()
+                #self.mouse.move_to(slots[0].random_point())
+                # Shift click any log to deposit all
+                #pag.keyDown("Shift")
+                #self.mouse.click()
+                #pag.keyUp("Shift")
+                #time.sleep(1) # give it a sec to register
+                #pag.keyDown("ESC") # close bank
+                #time.sleep(0.2)
+                #pag.keyUp("ESC")
+                #time.sleep(0.5) # give it a sec to close the bank UI
+
+
 
             # If our mouse isn't hovering over a tree, and we can't find another tree...
-            if not self.mouseover_text(contains="Chop") and not self.__move_mouse_to_nearest_tree():
-                failed_searches += 1
-                if failed_searches % 10 == 0:
-                    self.log_msg("Searching for trees...")
-                if failed_searches > 60:
-                    # If we've been searching for a whole minute...
-                    self.__logout("No tagged trees found. Logging out.")
-                    return
-                time.sleep(1)
-                continue
-            failed_searches = 0  # If code got here, a tree was found
+            #if not self.mouseover_text(contains="Chop") and not self.__move_mouse_to_nearest_tree():
+            #    failed_searches += 1
+            #    if failed_searches % 10 == 0:
+            #        self.log_msg("Searching for trees...")
+            #    if failed_searches > 60:
+            #        # If we've been searching for a whole minute...
+            #        self.__logout("No tagged trees found. Logging out.")
+            #        return
+            #    time.sleep(1)
+            #    continue
+            #failed_searches = 0  # If code got here, a tree was found
 
-            # Click if the mouseover text assures us we're clicking a tree
-            if not self.mouseover_text(contains="Chop"):
-                continue
-            self.mouse.click()
-            time.sleep(0.5)
+            # If not cutting, click tree
+
+            while not self.is_player_doing_action("Woodcutting"):
+                #self.take_break(max_seconds=1)
+                tree = self.get_nearest_tag(clr.PINK)
+                if tree is None:
+                    failed_searches += 1
+                    self.log_msg("Failed to find tree.")
+                    time.sleep(1)
+                    if failed_searches > 30:
+                        self.__logout("No tagged trees found. Logging out.")
+                        return
+                    break
+                else:
+                    print("move to!")
+                    self.mouse.move_to(tree.random_point())
+                    if not self.mouseover_text(contains="Chop"):
+                        print("No Chop")
+                        time.sleep(1)
+                        break
+                    self.log_msg("Clicking tree...")
+                    print("click!")
+                    pag.click()
+                    time.sleep(1)
+                    failed_searches = 0
+                    break
+             
+            while not api_m.get_is_player_idle():
+                time.sleep(1)
+                print("not idle")
+            print("idle")    
+            #notidle = 1
+            #time.sleep(3)
+
+
+
+
+
+           # # Click if the mouseover text assures us we're clicking a tree
+           # if not self.mouseover_text(contains="Chop"):
+           #     continue
+           # self.mouse.click()
+           # time.sleep(0.5)
 
             # While the player is chopping (or moving), wait
-            while not api_m.get_is_player_idle():
-                # Every second there is a 10% chance to move the mouse to the next tree
-                if rd.random_chance(probability=0.10):
-                    self.__move_mouse_to_nearest_tree(next_nearest=True)
-                time.sleep(1)
+           # while not api_m.get_is_player_idle():
+           #     # Every second there is a 10% chance to move the mouse to the next tree
+           #     if rd.random_chance(probability=0.10):
+           #         self.__move_mouse_to_nearest_tree(next_nearest=True)
+           #     time.sleep(1)
 
             self.update_progress((time.time() - start_time) / end_time)
 
         self.update_progress(1)
         self.__logout("Finished.")
+
+
+
 
     def __logout(self, msg):
         self.log_msg(msg)
@@ -128,13 +266,13 @@ class OSRSWoodcutter(OSRSBot):
             self.mouse.move_to(tree.random_point())
         return True
 
-    def __drop_logs(self, api_s: StatusSocket):
-        """
-        Private function for dropping logs. This code is used in multiple places, so it's been abstracted.
-        Since we made the `api` and `logs` variables assigned to `self`, we can access them from this function.
-        """
-        slots = api_s.get_inv_item_indices(ids.logs)
-        self.drop(slots)
-        self.logs += len(slots)
-        self.log_msg(f"Logs cut: ~{self.logs}")
-        time.sleep(1)
+        #def __drop_logs(self, api_s: StatusSocket):
+        
+         #   """Private function for dropping logs. This code is used in multiple places, so it's been abstracted.
+          #  Since we made the `api` and `logs` variables assigned to `self`, we can access them from this function.
+           # """
+            #slots = api_s.get_inv_item_indices(ids.logs)
+            #self.drop(slots)
+            #self.logs += len(slots)
+            #self.log_msg(f"Logs cut: ~{self.logs}")
+            #time.sleep(1) 
